d5e05d963340a29b6c0cc1a5d4127fd61e9331fc
milescm <milescm@nate.com>
2020-08-04 13:17:19 +0900

    commit

--- myapp/views.py
+++ myapp/views.py
@@ -34,15 +34,15 @@ def showdata(request):
 
 
 def readcsv(request):
-    # with open('/home/rtst15/Django_PTP_visual/f_csv/ptpTime10min_1.csv', 'r') as f:
-    #     dr = csv.DictReader(f)
-    #     s = pd.DataFrame(dr)
-    # ss = []
-    # for i in range(len(s)):
-    #     st = (s['__REALTIME_TIMESTAMP'][i], s['UTC'][i],s['Master Offset'][i],s['Frequency'][i],s['Path Delay'][i])
-    #     ss.append(st)
-    # for i in range(len(s)):
-    #     Csvdata.objects.create(realtime_timestamp=ss[i][0], utc=ss[i][1], master_offset=ss[i][2], frequency=ss[i][3], path_delay=ss[i][4])
+    with open('/home/rtst15/Django_PTP_visual/f_csv/ptpTime10min_1.csv', 'r') as f:
+        dr = csv.DictReader(f)
+        s = pd.DataFrame(dr)
+    ss = []
+    for i in range(len(s)):
+        st = (s['__REALTIME_TIMESTAMP'][i], s['UTC'][i],s['Master Offset'][i],s['Frequency'][i],s['Path Delay'][i])
+        ss.append(st)
+    for i in range(len(s)):
+        Csvdata.objects.create(realtime_timestamp=ss[i][0], utc=ss[i][1], master_offset=ss[i][2], frequency=ss[i][3], path_delay=ss[i][4])
 
     data = Csvdata.objects.all().values()
     return render(request, 'myapp/readcsv.html', {'data': data})
