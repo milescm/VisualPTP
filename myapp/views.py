601c0da9c11a9f1b7b9c95bc2a12ca38ba05736c
milescm <milescm@nate.com>
2020-08-07 14:20:10 +0900

    Update user's form, enable to upload csv

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import random
 from .models import Csvdata
-import csv
+import csv, io
 import pandas as pd
+from django.db import IntegrityError
+from django.http import JsonResponse
+from django.contrib import messages
+
 
 def home(request):
     return render(request, 'myapp/home.html')
@@ -85,3 +89,61 @@ def showstock(request):
         'timestamp': timestamp
     })
 
+def userform(request):
+    return render(request, 'myapp/userform.html')
+
+def filecheck(request):
+    if Csvdata.objects.count() != 0:
+        message = "The Data already exits!"
+        return JsonResponse({"message": message})
+    else:
+        if request.FILES.__len__()==0:
+            message = "No uplaod file"
+            return JsonResponse({"message": message})
+        else:
+            uploadFile = request.FILES['file']
+            if uploadFile.name.find('csv')<0:
+                message = "This file is not csv file"
+                return JsonResponse({"message": message})
+            else:
+                read = uploadFile.read().decode('utf8')
+                readLine = read.split('\n')
+
+                tmp_str = []
+
+                       
+                for line in readLine:
+                    tmp_str.append(line.split(','))
+                
+                for i in range(1,len(tmp_str)-1):
+                    Csvdata.objects.create(realtime_timestamp=tmp_str[i][1], utc=tmp_str[i][2],master_offset=tmp_str[i][3],frequency=tmp_str[i][4],path_delay=tmp_str[i][5])
+                    # print(tmp_str[i][3])
+                # print(tmp_str[0][0])
+                # print(tmp_str[1][0])
+                # print(tmp_str[2][0])
+                # print(tmp_str[10][0])
+
+                # for i in arr:
+                #     print(i[0][1])
+                # # print(arr)
+                message = "Nice csv file and read completely"
+                return render(request, 'myapp/filecheck.html')
+
+
+
+
+
+        #         with open('/home/rtst15/Django_PTP_visual/f_csv/ptpTime30min_1.csv', 'r') as f:
+        #     dr = csv.DictReader(f)
+        #     s = pd.DataFrame(dr)
+        # ss = []
+        # for i in range(len(s)):
+        #     st = (s['__REALTIME_TIMESTAMP'][i], s['UTC'][i],s['Master Offset'][i],s['Frequency'][i],s['Path Delay'][i])
+        #     ss.append(st)
+        # for i in range(len(s)):
+        #     Csvdata.objects.create(realtime_timestamp=ss[i][0], utc=ss[i][1], master_offset=ss[i][2], frequency=ss[i][3], path_delay=ss[i][4])
+
+        # data = Csvdata.objects.all().values()
+        # return render(request, 'myapp/readcsv.html', {
+        #     'data': data
+        #         })
